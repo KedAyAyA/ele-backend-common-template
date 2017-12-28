29d0063dfd8509497a2cf6365369c97b856734fb
KedAyAyA <caoxun333@gmail.com>
2017-12-28 10:13:09 +0800

    add reset.scss

--- src/App.vue
+++ src/App.vue
@@ -10,35 +10,8 @@ export default {
 }
 </script>
 
-<style>
-html,
-body {
-	margin: 0;
-	padding: 0;
-	font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
-  font-size: 14px;
-  height: 100%;
-  width: 100%;
-	-webkit-font-smoothing: antialiased;
-}
-
-section,
-div,
-aside,
-header {
-  box-sizing: border-box;
-}
-
-.el-menu-item [class^=fa],
-.el-submenu [class^=fa] {
-  color: #fff;
-  vertical-align: baseline;
-  margin-right: 10px;
-}
-
-.el-submenu__title i {
-  color: #fff;
-}
+<style lang="scss">
+@import "~scss/reset.scss";
 
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
